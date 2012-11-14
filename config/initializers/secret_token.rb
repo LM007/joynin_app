144a7fa188f1ef83be245e44f510b6ed42119b65
leemarshall <lpdmarshal@gmail.com>
2012-11-14 21:25:59 +0000

    joynin first commit, app directory and enviroment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JoyninApp::Application.config.secret_token = '3ee37f2240095663d5828e1189c3139ec41a5ff08499e38b078cf712a664ce94d5541efb784a4062b0e009222ce373dce7f4177bdd5ea334befb2d5df351e63a'
